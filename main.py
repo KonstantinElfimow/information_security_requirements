e08da681e46079c617a1da311e21fa8594b07abe
Konstantin <k.elfimow@gmail.com>
2023-05-13 22:48:18 +0300

    Парсю сайт

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import tkinter as tk
 from tkinter import ttk
 import pandas as pd
@@ -6,31 +7,59 @@ from bs4 import BeautifulSoup
 import json
 import csv
 
-# url = 'https://fstec.ru/dokumenty/vse-dokumenty/prikazy/prikaz-fstek-rossii-ot-11-fevralya-2013-g-n-17'
-#
-# headers = {
-#     'accept': '*/*',
-#     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 '
-#                   'YaBrowser/23.3.1.895 Yowser/2.5 Safari/537.36 '
-# }
-#
-# # Отключение проверки сертификата SSL
-# requests.packages.urllib3.disable_warnings()
-# req = requests.get(url, headers=headers, verify=False)
-# src = req.text
-#
-# with open('index.html', mode='w', encoding='utf-8') as file:
-#     file.write(src)
+url = 'https://fstec.ru/dokumenty/vse-dokumenty/prikazy/prikaz-fstek-rossii-ot-11-fevralya-2013-g-n-17'
 
-with open('index.html', mode='r', encoding='utf-8') as file:
-    src = file.read()
+headers = {
+    'accept': '*/*',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 '
+                  'YaBrowser/23.3.1.895 Yowser/2.5 Safari/537.36 '
+}
 
-soup = BeautifulSoup(src, 'html.parser')
+file_path_html = 'index.html'
+if not os.path.isfile(file_path_html):
+    # Отключение проверки сертификата SSL
+    requests.packages.urllib3.disable_warnings()
+    req = requests.get(url, headers=headers, verify=False)
+    src = req.text
 
-# all_rows = soup.find_all(class_='mzr-tc-group-item-href')
+    with open(file_path_html, mode='w', encoding='utf-8') as file:
+        file.write(src)
+del file_path_html
 
-df_1 = pd.read_csv('information_security_class.csv', delimiter=';')
-df_2 = pd.read_csv('security_class_requirements.csv', delimiter=';')
+file_path_isc = 'data/information_security_class.csv'
+file_path_scr = 'data/security_class_requirements.csv'
+if not os.path.isfile(file_path_isc) or not os.path.isfile(file_path_scr):
+    with open('index.html', mode='r', encoding='utf-8') as file:
+        src = file.read()
+
+    soup = BeautifulSoup(src, 'lxml')
+
+    tables = soup.find_all('table', class_='sltable')
+
+    if True:
+        table_isc = tables[0]
+        table_rows = table_isc.find('tbody').find_all('tr')
+
+        data = []
+        for row in table_rows:
+            attr_values = row.find_all('td')
+
+            v1 = attr_values[0].text.replace(' ', ' ').strip()
+            v2 = attr_values[1].text.strip()
+            v3 = attr_values[2].text.strip()
+            v4 = attr_values[3].text.strip()
+
+            data.append([v1, v2, v3, v4])
+
+        with open(f'data/information_security_class.csv', mode='w', encoding='utf-8-sig', newline='') as csvfile:
+            writer = csv.writer(csvfile, delimiter=';')
+            columns = ['уровень_значимости', 'федеральный', 'региональный', 'объектовый']
+            writer.writerow(columns)
+            writer.writerows(data)
+
+exit()
+df_isc = pd.read_csv('information_security_class.csv', delimiter=';')
+df_scr = pd.read_csv('security_class_requirements.csv', delimiter=';')
 
 
 def show_security_class_requirements(*, significance_level: str, scale: str):
@@ -54,8 +83,8 @@ def show_security_class_requirements(*, significance_level: str, scale: str):
             'федеральный', 'региональный', 'объектовый'):
         text.insert(tk.END, 'Введены неверные данные!')
     else:
-        security_class = df_1.loc[df_1['уровень_значимости'] == significance_level, scale].values[0]
-        requirements = (df_2.loc[df_2[security_class] == 1, ['номер_меры', 'мера_защиты']]).to_string()
+        security_class = df_isc.loc[df_isc['уровень_значимости'] == significance_level, scale].values[0]
+        requirements = (df_scr.loc[df_scr[security_class] == 1, ['номер_меры', 'мера_защиты']]).to_string()
         text.insert(tk.END,
                     'Ваш класс защищённости:\n{}\nТребования к системе:\n{}'.format(security_class, requirements))
 
